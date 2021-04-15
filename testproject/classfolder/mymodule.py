3508f12abd0f0f02fbaefd6a1db64eafec82ae35
JK696X <jk696x@att.com>
2021-04-15 13:24:47 -0500

    Added a virtual environment and tested module creation
    Added a virtual environment to test off of, along with a module we can pull class data from.

---
+++ testproject/classfolder/mymodule.py
@@ -0,0 +1,4 @@
+def myModuleGreeting():
+    print(
+        "Hello, welcome to my spooky module in an unknown folder you've never opened before!"
+    )
--- testproject/script.py
+++ testproject/script.py
@@ -31,20 +31,23 @@ import math
 # See above to install this package beforehand
 import requests
 
-print(sys.version)  # prints version
-print(sys.executable)  # prints location where Python is installed
-print(os.getcwd())  # get the working directory
+# print(sys.version)  # prints version
+# print(sys.executable)  # prints location where Python is installed
+# print(os.getcwd())  # get the working directory
 
 # import a class from a directory we are running main in
-# import classtest
+import classtest
+
 # from classtest import myClass
 
 # import our class from a different directory; this is used in the example below
+print(sys.path)
+from classfolder.mymodule import myModuleGreeting as myGreeting
 
 # These are all the directories python will look in to import directories
-print()
-print(sys.path)
-print()
+# print()
+# print(sys.path)
+# print()
 # This is one way to add a module to your syspath...but it's not the BEST way
 # sys.path.append('D:\Python Programming\Python Programming\example_all_project\otherclasses')
 
@@ -55,14 +58,14 @@ print()
 # from car import Car as Car #import this file
 
 # define a function
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
-print(greet("World"))
-print(greet("Joe"))
-print("")
+# print(greet("World"))
+# print(greet("Joe"))
+# print("")
 
 # This is an example package from the 'requests' package.
 # To learn more about this, right click on any section,('get', for example)
@@ -70,28 +73,28 @@ print("")
 # Make sure you have no issues with VPN or whatever as well!
 # python command in terminal:
 # try: pip install requests
-try:
-    r = requests.get("http://josephkeller.me/")
-    print(r.status_code)
-except Exception as e:
-    print("Could not reach your resume site! {}", format(e))
+# try:
+#     r = requests.get("http://josephkeller.me/")
+#     print(r.status_code)
+# except Exception as e:
+#     print("Could not reach your resume site! {}", format(e))
 
-# Read from file example
-f = open(
-    "guru99.txt", "a+"
-)  # 'a+' means append, and the plus creates the file if it dosen't exist
-# append 2 lines
-for i in range(2):
-    f.write("Appended line %d\r\n" % (i + 1))
-# close file
-f.close()
-# open file again and read from it
-g = open("guru99.txt", "r")
-for x in g:
-    print(x)
+# # Read from file example
+# f = open(
+#     "guru99.txt", "a+"
+# )  # 'a+' means append, and the plus creates the file if it dosen't exist
+# # append 2 lines
+# for i in range(2):
+#     f.write("Appended line %d\r\n" % (i + 1))
+# # close file
+# f.close()
+# # open file again and read from it
+# g = open("guru99.txt", "r")
+# for x in g:
+#     print(x)
 
-# close the file when finished reading/writing
-f.close()
+# # close the file when finished reading/writing
+# f.close()
 
 # This allows you to exit the program; it will not run anytghing beneth it
 # you must import sys
@@ -106,7 +109,6 @@ f.close()
 # nicholas.get_student_details()
 
 # Test with classes in another directory
+# This was defined way up top!
 # Example: https://www.youtube.com/watch?v=X1cwEKfRZJEThan
-# otherclasses.greetingcool("David")
-# coolVariableClass = otherclasses.myCoolClass("Example string")
-# print(coolVariableClass.getVal() + " is the value here")
+myGreeting()
